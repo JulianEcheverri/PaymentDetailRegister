efbc42eb0d9d65208b0f2fa439358c954ba65710
JulianEcheverri <julianecheverri@outlook.com>
2020-05-12 12:48:54 -0500

    Creamos la clase de pagos que hace de entidad para luego ser agregada al DBConttest

---
+++ PaymentDetailRegister/Models/PaymentDetail.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PaymentDetailRegister.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PMId { get; set; }
+
+        [Required, StringLength(100)]
+        public string CardOwnerName { get; set; }
+
+        [Required, StringLength(16)]
+        public string CardNumber { get; set; }
+
+        [Required, StringLength(5)]
+        public string ExpirationDate { get; set; }
+
+        [Required, StringLength(3)]
+        public string CVV { get; set; }
+    }
+}
