6d819c588d4ae939dd279948a940d03ab2ea48a3
JulianEcheverri <julianecheverri@outlook.com>
2020-05-12 13:17:23 -0500

    Realizamos la migración para crear la db y sus tablas Cambiamos el string de conexión ya que no tenia el servidor correcto

---
+++ PaymentDetailRegister/Migrations/20200512181045_MigracionInicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentDetailRegister.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PMId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOwnerName = table.Column<string>(maxLength: 100, nullable: false),
+                    CardNumber = table.Column<string>(maxLength: 16, nullable: false),
+                    ExpirationDate = table.Column<string>(maxLength: 5, nullable: false),
+                    CVV = table.Column<string>(maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PMId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+        }
+    }
+}
