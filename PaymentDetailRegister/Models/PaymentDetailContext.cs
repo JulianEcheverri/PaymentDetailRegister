3ca41d537b66d1d8521ff77a8841134e6601b510
JulianEcheverri <julianecheverri@outlook.com>
2020-05-12 13:06:10 -0500

    Añadimos la clas de contexto la cual decide que tendrá la base de datos Añadimos el servicio en Starup como inyeción de dependencia para usar el contexto en toda la aplicación Añadimos el string de conexión en appsettings.json

---
+++ PaymentDetailRegister/Models/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentDetailRegister.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        // En el constructor debemos pasarle que base de datos usaremos y el string de conexion. Se hace mediante el parametro DbContextOptions options
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        { }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+
+}
--- PaymentDetailRegister/Startup.cs
+++ PaymentDetailRegister/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PaymentDetailRegister.Models;
 
 namespace PaymentDetailRegister
 {
@@ -25,6 +27,9 @@ namespace PaymentDetailRegister
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            // Agregamos el servicio (Dependency Injection) pasandole el motor de bases de datos que usaremos y el string de conexion
+            // Configuration.GetConnectionString("DevConnection") consulta en el json 'appsettings.json' el string de conexion, el cual esta apuntando con el nombre DevConnection
+            services.AddDbContext<PaymentDetailContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
