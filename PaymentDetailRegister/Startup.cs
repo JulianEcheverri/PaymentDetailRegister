31ab29821e509d869f072f9e784f3b919fca68ef
JulianEcheverri <julianecheverri@outlook.com>
2020-05-12 22:26:33 -0500

    Añadimos libreria de cors en el proyecto .net y permitimos el acceso a la api rest

--- PaymentDetailRegister/Startup.cs
+++ PaymentDetailRegister/Startup.cs
@@ -26,10 +26,21 @@ namespace PaymentDetailRegister
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            // Por defecto, .Net tiene el formato json con el camel case para las respuestas en sus objetos, los devuelve asi cardNumber
+            // Se realiza esta porcion de codigo para evitar que eso suceda
+            services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+                options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+            });
+
+
             // Agregamos el servicio (Dependency Injection) pasandole el motor de bases de datos que usaremos y el string de conexion
             // Configuration.GetConnectionString("DevConnection") consulta en el json 'appsettings.json' el string de conexion, el cual esta apuntando con el nombre DevConnection
             services.AddDbContext<PaymentDetailContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            // .Net va a bloquear peticiones que esten fuera de su dominio o puerto, para ello instalamos el paquete Microsoft.AspNetCore.Cors y lo añadimos en el startup
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +59,9 @@ namespace PaymentDetailRegister
             {
                 endpoints.MapControllers();
             });
+
+            // Añadimos los origenes permitidos de dominios externos
+            app.UseCors(options => options.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader());
         }
     }
 }
